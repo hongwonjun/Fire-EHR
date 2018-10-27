89b12494cf5a460fe0f964442cb6cd437b53f8d9
hurcy <cinyoung.hur@gmail.com>
2018-10-27 19:44:39 +0900

    Add patient list and search api

--- ehr/controllers/patient.py
+++ ehr/controllers/patient.py
@@ -3,6 +3,9 @@ from flask import Blueprint, request, jsonify, make_response
 from flask_cors import cross_origin
 import fhirclient.models.patient as p
 import fhirclient.models.humanname as hn
+import fhirclient.models.fhirdate as fd  # .FHIRDate as fhirdate
+import fhirclient.models.address as addr
+from datetime import datetime
 
 from ehr import (
     app,
@@ -16,6 +19,33 @@ patient_route = Blueprint(
 )
 
 
+@patient_route.route("/list", methods=["GET"])
+def patient_list():
+    result = db.engine.execute("select * from T_Patients limit 10")
+    plist = []
+    for row in result:
+        patient = p.Patient({'id': row['pat_id']})
+
+        name = hn.HumanName()
+        name.given = [row['pat_first']]
+        name.family = row['pat_last']
+        patient.name = [name]
+
+        address = addr.Address()
+        address.city = row['pat_city']
+        address.state = row['pat_state']
+        address.postalCode = row['pat_zip']
+        address.district = row['pat_address']
+        patient.address = [address]
+
+        patient.gender = row['pat_gender']
+        patient.birthDate = fd.FHIRDate(
+            datetime.strftime(row['pat_birthdate'], "%Y-%m-%d"))
+        # patient.maritalStatus = row['pat_marital']
+        plist.append(patient.as_json())
+    return jsonify(result=plist)
+
+
 @patient_route.route("/<id>", methods=["GET"])
 def patient(id):
     # https://{FHIR Base URL}/Patient/{id}?
@@ -25,20 +55,30 @@ def patient(id):
     # _format=json
 
     # TODO: query params
-    # name = request.args.get('name', None)
+    name = request.args.get('name', None)
+    family = request.args.get('family', None)
+    gender = request.args.get('gender', None)
 
     # TODO: connect to database to search patient
-    result = db.engine.execute("SELECT 1")
+
+    result = db.engine.execute("select * from T_Patients limit 1")
     names = []
     for row in result:
-        pass
+        patient = p.Patient({'id': row['pat_id']})
+        name = hn.HumanName()
+        name.given = [row['pat_first']]
+        name.family = row['pat_last']
+        patient.name = [name]
 
-    # dummy data
-    patient = p.Patient({'id': str(id)})
-    name = hn.HumanName()
-    name.given = ['Peter']
-    name.family = 'Parker'
-    patient.name = [name]
-    print(patient.as_json())
+        address = addr.Address()
+        address.city = row['pat_city']
+        address.state = row['pat_state']
+        address.postalCode = row['pat_zip']
+        address.district = row['pat_address']
+        patient.address = [address]
 
+        patient.gender = row['pat_gender']
+        patient.birthDate = fd.FHIRDate(
+            datetime.strftime(row['pat_birthdate'], "%Y-%m-%d"))
+        patient.maritalStatus = row['pat_marital']
     return jsonify(result=patient.as_json())
