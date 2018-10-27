181ee9df1e64184a765c0f71d225e6ef45b25fea
Wonjun Hong <hong.wonjun@gmail.com>
2018-10-27 21:15:23 +0900

    환자 리스트 기본형

--- viz/pages/index.js
+++ viz/pages/index.js
@@ -1,36 +1,132 @@
 import React from "react"
 import ApiClient from "../helpers/apiClient"
 import DemoPage from "../layouts/demoPage"
+import Pagination from "../components/Pagination"
 import LineChart from "../components/charts/line"
 
 class Demo extends React.Component {
+  static getInitialProps(ctx) {
+    var apiClient = new ApiClient({
+      path: "Patient/list"
+    })
+
+    var page = ctx.query.page ? parseInt(ctx.query.page) : 1
+    var length = ctx.query.length ? parseInt(ctx.query.length) : 18
+
+    return apiClient.get({ start: page - 1, length: length }).then(
+      response => {
+        return Promise.resolve({
+          page: page,
+          length: length,
+
+          list: response.data.results,
+          total: response.data.total
+        })
+      },
+      error => {
+        if (error.error.response.status === 400) {
+          return Promise.resolve({
+            error: 400
+          })
+        }
+        console.log("GET FAILED")
+        return Promise.reject("Failed")
+      }
+    )
+  }
+
   constructor(props) {
     super(props)
     this.state = {}
   }
+  
   componentDidMount = () => {
-    this.samplePatient()
+    
   }
 
-  samplePatient = () => {
-    return new ApiClient({
-      path: "Patient/1"
-    })
-      .get()
-      .then(
-        response => {
-          console.log(response.data)
-          return response
-        },
-        error => {
-          return Promise.reject("Failed")
-        }
-      )
-  }
   render() {
     return (
       <DemoPage url={this.props.url.pathname}>
-        <div> Hello World </div>
+        <div className="container-fluid">
+          <div className="row" style={{ marginTop: "5px" }}>
+            <div className="col-lg-12">
+              <div className="ibox">
+                <div className="ibox-title">
+                  <h3>
+                    Patient List
+                  </h3>
+                </div>
+                <div className="ibox-content">
+                  <div className="row">
+                    <div className="col-lg-8">
+                      <div className="row">
+                        <div className="table-responsive">
+                          <table className="table table-striped">
+                            <colgroup>
+                              <col width="30%" />
+                              <col width="30%" />
+                              <col width="10%" />
+                              <col width="10%" />
+                              <col width="20%" />
+                            </colgroup>
+                            <thead>
+                              <tr>
+                                <th>Patient ID</th>
+                                <th>Name</th>
+                                <th>Gender</th>
+                                <th>BirthDate</th>
+                                <th></th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {this.props.list.map((info, idx) => {
+                                return (
+                                  <tr key={idx}>
+                                    <td>{info.id}</td>
+                                    <td>{info.name[0].family}</td>
+                                    <td>{info.gender}</td>
+                                    <td>{info.birthDate}</td>
+                                    <td>
+                                      <a
+                                        type="button"
+                                        className="btn btn-primary"
+                                        href=""
+                                      >
+                                        Timeline
+                                      </a>
+                                    </td>
+                                  </tr>
+                                )
+                              })}
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-lg-12">
+                          <div className="text-center">
+                            <Pagination
+                              currentIndex={this.props.page}
+                              totalItems={this.props.total}
+                              pageLink={
+                                this.props.url.pathname +
+                                "?length=" +
+                                this.props.length +
+                                "&page="
+                              }
+                              itemsPerPage={this.props.length}
+                              showPagesCount={10}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </DemoPage>
     )
   }
